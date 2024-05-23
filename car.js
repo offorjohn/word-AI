80f91c11d7cd8c16ea9a9b778ee4bc6c6dcdb66b
offor <offorchukwuebuka8@gmail.com>
2024-05-23 17:13:16 +0100

    first commit

--- car.js
+++ car.js
@@ -199,3 +199,18 @@ class Car {
 
     }
 }
+
+document.addEventListener("click", () =>{
+    if (document.body.requestFullscreen){
+        document.body.requestFullscreen();
+    
+    } else if (document.body.mozRequestFullScreen){
+        document.body.mozRequestFullScreen();
+
+    } else if (document.body.webkitRequestFullscreen){
+        document.body.webkitRequestFullscreen();
+
+    } else if (document.body.msRequestFullscreen) {
+        document.body.msRequestFullscreen();
+    }
+})
