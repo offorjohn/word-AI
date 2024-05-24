dede1a6732221febc804543a970dd3872eb1e794
offor <offorchukwuebuka8@gmail.com>
2024-05-24 12:06:13 +0100

    first commit

--- camera.js
+++ camera.js
@@ -1,18 +1,18 @@
 class Camera {
-    constructor({ x, y, angle}, range = 1000, distanceBehind = 150) {
-        
+    constructor({ x, y, angle }, range = 1000, distanceBehind = 100) {
+
         this.range = range;
         this.distanceBehind = distanceBehind;
         this.moveSimple({ x, y, angle });
     }
 
-    move({ x, y, angle}){
-        const t = 0.1;
+    move({ x, y, angle }) {
+        const t = 0.15;
         this.x = lerp(this.x, x + this.distanceBehind * Math.sin(angle), t);
         this.y = lerp(this.y, y + this.distanceBehind * Math.cos(angle), t);
         this.z = -40;
-        this.angle =  lerp(this.angle, angle, t);
-      
+        this.angle = lerp(this.angle, angle, t);
+
         this.center = new Point(this.x, this.y);
         this.tip = new Point(
             this.x - this.range * Math.sin(this.angle),
@@ -32,12 +32,12 @@ class Camera {
 
     }
 
-    moveSimple({ x, y, angle}){
+    moveSimple({ x, y, angle }) {
         this.x = x + this.distanceBehind * Math.sin(angle);
         this.y = y + this.distanceBehind * Math.cos(angle);
         this.z = -40;
         this.angle = angle;
-      
+
         this.center = new Point(this.x, this.y);
         this.tip = new Point(
             this.x - this.range * Math.sin(this.angle),
@@ -59,27 +59,27 @@ class Camera {
 
 
     #projectPoint(ctx, p) {
-       const seg  = new Segment(this.center, this.tip);
-       const { point: p1 } = seg.projectPoint(p);
-       const c = cross(subtract(p1, this), subtract(p, this));
-       const x = Math.sign(c) * distance(p, p1) / distance(this, p1);
-       const y = (p .z - this.z) / distance(this, p1);
+        const seg = new Segment(this.center, this.tip);
+        const { point: p1 } = seg.projectPoint(p);
+        const c = cross(subtract(p1, this), subtract(p, this));
+        const x = Math.sign(c) * distance(p, p1) / distance(this, p1);
+        const y = (p.z - this.z) / distance(this, p1);
 
-       const cX = ctx.canvas.width / 2;
-       const cY = ctx.canvas.height / 2;
-       const scaler = Math.max(cX, cY);
-       return new Point(cX + x * scaler, cY + y * scaler);
+        const cX = ctx.canvas.width / 2;
+        const cY = ctx.canvas.height / 2;
+        const scaler = Math.max(cX, cY);
+        return new Point(cX + x * scaler, cY + y * scaler);
 
-       } 
+    }
 
-       #filter(polys) {
+    #filter(polys) {
         const filteredPolys = [];
-        for (const poly of polys ) {
-          
+        for (const poly of polys) {
+
             if (poly.intersectsPoly(this.poly)) {
                 const copy1 = new Polygon(poly.points);
                 const copy2 = new Polygon(this.poly.points);
-                Polygon.break(copy1, copy2, true );
+                Polygon.break(copy1, copy2, true);
                 const points = copy1.segments.map((s) => s.p1);
                 const filteredPoints = points.filter(
                     (p) => p.intersection || this.poly.containsPoint(p)
@@ -91,11 +91,11 @@ class Camera {
         }
         return filteredPolys;
 
-       }
+    }
 
-       #extrude(polys, height = 10) {
-         const extrudedPolys = [];
-         for (const poly of polys) {
+    #extrude(polys, height = 10) {
+        const extrudedPolys = [];
+        for (const poly of polys) {
             const ceiling = new Polygon(
                 poly.points.map((p) => new Point(p.x, p.y, -height))
 
@@ -111,11 +111,163 @@ class Camera {
                 ]));
             }
             extrudedPolys.push(...sides, ceiling);
-         }
-         return extrudedPolys;
-       }
+        }
+        return extrudedPolys;
+    }
 
-       #getPolys(world) {
+    #extrudeCar(poly, height = 20, wheelRadius = 5) {
+        const frontRight = new Point(poly.points[0].x, poly.points[0].y);
+        const frontLeft = new Point(poly.points[1].x, poly.points[1].y);
+        const backLeft = new Point(poly.points[2].x, poly.points[2].y);
+        const backRight = new Point(poly.points[3].x, poly.points[3].y);
+        const middleLeft = new Point(
+            (frontLeft.x + backLeft.x) / 2,
+            (frontLeft.y + backLeft.y) / 2
+        );
+        const middleRight = new Point(
+            (frontRight.x + backRight.x) / 2,
+            (frontRight.y + backRight.y) / 2
+        );
+        
+         const quarterFrontLeft = average(frontLeft, middleLeft);
+         const quarterBackLeft  = average(backLeft, middleLeft);
+         const quarterFrontRight = average(frontRight, middleRight);
+         const quarterBackRight = average(backRight, middleRight)
+         this.#moveInward(frontLeft, frontRight, 0.2);
+         this.#moveInward(backLeft, backRight, 0.1);
+    
+        const base = new Polygon([
+            frontLeft,
+            middleLeft,
+            quarterFrontLeft,
+            //middleLeft,
+            quarterBackLeft,
+            backLeft,
+            backRight,
+            middleRight,
+            quarterBackRight,
+           // middleRight,
+            quarterFrontRight,
+            frontRight
+        ]);
+    
+        for (const point of base.points) {
+            point.z -= wheelRadius;
+        }
+    
+        const ceiling = new Polygon(
+            base.points.map((p) => new Point(p.x, p.y, -height))
+        );
+
+        
+        const midLine = new Polygon(
+            base.points.map((p) => new Point(p.x, p.y, -height / 2))
+        );
+
+        const c_frontLeft = ceiling.points[0];
+        const c_middleLeft = ceiling.points[1];
+        const c_quarterFrontLeft = ceiling.points[2];
+        //middleLeft,
+        const c_quarterBackLeft = ceiling.points[3];
+        const c_backLeft = ceiling.points[4];
+        const c_backRight = ceiling.points[5];
+        const c_middleRight = ceiling.points[6];
+        const c_quarterBackRight = ceiling.points[7];
+       // middleRight,
+       const c_quarterFrontRight = ceiling.points[8];
+       const c_frontRight  = ceiling.points[9];
+
+       c_frontLeft.z += 7;
+       c_frontRight.z += 7;
+       c_quarterFrontLeft.z += 6;
+       c_quarterFrontRight.z += 6;
+       c_backLeft.z += 4;
+       c_backRight.z += 4;
+
+       this.#moveInward(c_frontLeft, c_frontRight);
+       this.#moveInward(c_quarterFrontLeft, c_quarterFrontRight);
+       this.#moveInward(c_middleLeft, c_middleRight);
+       this.#moveInward(c_quarterBackLeft, c_quarterBackRight);
+       this.#moveInward(c_backLeft, c_backRight);
+       this.#moveInward(c_frontLeft, c_backLeft, 0.1);
+       this.#moveInward(c_frontRight, c_backRight, 0.1);
+
+
+
+        const sides = [];
+        for (let i = 0; i < base.points.length; i++) {
+            sides.push(new Polygon([
+                base.points[i],
+                base.points[(i + 1) % base.points.length],
+                midLine.points[(i + 1) % midLine.points.length],
+                midLine.points[i]
+            ]));
+        }
+
+        for (let i = 0; i < base.points.length; i++) {
+            sides.push(new Polygon([
+                midLine.points[i],
+                midLine.points[(i + 1) % midLine.points.length],
+                ceiling.points[(i + 1) % ceiling.points.length],
+                ceiling.points[i]
+            ]));
+        }
+
+
+
+
+        const ceilingParts = [];
+
+        ceilingParts.push(new Polygon([
+            c_frontLeft,
+            c_quarterFrontLeft,
+            c_quarterBackRight,
+            c_frontRight
+        ]));
+
+        
+        ceilingParts.push(new Polygon([
+          
+            c_quarterFrontLeft,
+            c_middleLeft,
+            c_middleRight,
+            c_quarterFrontRight
+      
+        ]));
+
+        
+        ceilingParts.push(new Polygon([
+           c_middleLeft,
+           c_quarterBackLeft,
+           c_quarterBackRight,
+           c_middleRight
+        ]));
+
+        
+        ceilingParts.push(new Polygon([
+          c_quarterBackLeft,
+          c_backLeft,
+          c_backRight,
+          c_quarterBackRight
+        ]))
+        
+
+
+        return [...sides, ...ceilingParts];
+    }
+
+    #moveInward(p1, p2, percent = 0.3) {
+        const new_p1 = lerp2D(p1, p2, percent);
+        const new_p2 = lerp2D(p2, p1, percent);
+        p1.x = new_p1.x;
+        p1.y = new_p1.y;
+        p2.x = new_p2.x;
+        p2.y = new_p2.y;
+
+    }
+    
+
+    #getPolys(world) {
         const buildingPolys = this.#extrude(this.#filter(
             world.buildings.map((b) => b.base)
         ), 200);
@@ -126,36 +278,54 @@ class Camera {
         ), 10);
 
 
-        const carPolys = this.#extrude(this.#filter(
-                [new Polygon(world.bestCar.polygon.map((p) => new Point(p.x, p.y)))]
+        const carPolys = this.#extrudeCar(this.#filter(
+            [new Polygon(world.bestCar.polygon.map((p) => new Point(p.x, p.y)))]
+        )[0]);
 
-            
-        ), 10);
-        return  [...buildingPolys, ...carPolys, ...roadPolys];
-        
+        const carShadows = this.#filter(
+            world.cars.map(
+                (c) => new Polygon(c.polygon.map((p) => new Point(p.x, p.y)))
 
+            )
 
-       }
+        );
 
-    render(ctx, world){
-       
+        for (const poly of carShadows) {
+            poly.fill = "rgba(150, 150, 150, 1)";
+            poly.stroke = "rgba(0, 0, 0, 0)";
+        }
+
+        for (const poly of buildingPolys) {
+            poly.fill = "rgba(150, 150, 150, 0.8)";
+            poly.stroke = "rgba(150, 150, 150, 0.8)";
+        }
+
+        return [...carShadows, ...buildingPolys, ...carPolys, ...roadPolys];
+
+    }
+
+    render(ctx, world) {
+
         const polys = this.#getPolys(world);
-        
+
         const projPolys = polys.map(
             (poly) => new Polygon(
-               poly.points.map((p) => this.#projectPoint(ctx, p))
-               )
-            );
+                poly.points.map((p) => this.#projectPoint(ctx, p))
+            )
+        );
 
-            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-            for (const poly of projPolys) {
-                poly.draw(ctx);
-            }
+        for (let i = 0; i < projPolys.length; i++) {
+            const { fill, stroke } = polys[i];
+            const dist = polys[i].distanceToPoint(this);
+            ctx.globalAlpha = (1 - dist / this.range) ** 2;
+            projPolys[i].draw(ctx, { fill, stroke, join: "round" });
+        }
 
     }
 
     draw(ctx) {
-      this.poly.draw(ctx);
+        this.poly.draw(ctx);
     }
 }
--- race.js
+++ race.js
@@ -1,104 +1,94 @@
-const rightPanelWidth = 300;
+﻿const rightPanelWidth = 300;
 
-document.body.style.flexDirection = 'column';
+document.body.style.flexDirection = "column";
 
-const carCanvas = document.getElementById("carCanvas");
-carCanvas.width = window.innerWidth;
-carCanvas.height=0; // window.innerHeight / 2;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=window.innerWidth;
+carCanvas.height=0;//window.innerHeight / 2;
 
-const cameraCanvas = document.getElementById("cameraCanvas");
-cameraCanvas.width = window.innerWidth;
-cameraCanvas.height = window.innerHeight;
+const cameraCanvas=document.getElementById("cameraCanvas");
+cameraCanvas.width=window.innerWidth;
+cameraCanvas.height=window.innerHeight;
 
-const miniMapCanvas = document.getElementById("miniMapCanvas");
-miniMapCanvas.width = rightPanelWidth;
-miniMapCanvas.height = rightPanelWidth;
-
+const miniMapCanvas=document.getElementById("miniMapCanvas");
+miniMapCanvas.width=rightPanelWidth;
+miniMapCanvas.height=rightPanelWidth;
 
 statistics.style.width = rightPanelWidth + "px";
 statistics.style.height = window.innerHeight - rightPanelWidth - 60 + "px";
 
-
 const carCtx = carCanvas.getContext("2d");
-
-
 const cameraCtx = cameraCanvas.getContext("2d");
 
-
-
 const viewport = new Viewport(carCanvas, world.zoom, world.offset);
-const miniMap = new MiniMap(miniMapCanvas, world.graph, rightPanelWidth);
 
-const N = 100;
-const cars = generateCars(1, "KEYS").concat(generateCars(N, "AI"));
-const myCar = cars[0];
+const N=100;
+const cars=generateCars(1, "KEYS").concat(generateCars(N, "AI"));
+const myCar=cars[0];
 const camera = new Camera(myCar);
 
-
-if (localStorage.getItem("bestBrain")) {
-    for (let i = 0; i < cars.length; i++) {
-        cars[i].brain = JSON.parse(
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(
             localStorage.getItem("bestBrain"));
-        if (i != 1) {
-            NeuralNetwork.mutate(cars[i].brain, 0.1);
+        if(i>1){
+            NeuralNetwork.mutate(cars[i].brain,0.1);
         }
     }
 }
 
 for (let i = 0; i < cars.length; i++) {
-    const div = document.createElement("div");
-    div.id = "stat_" + i;
-    div.innerText = i;
-    div.style.color = cars[i].color;
-    div.classList.add("stat");
-    statistics.appendChild(div);
+   const div = document.createElement("div");
+   div.id = "stat_" + i;
+   div.innerText = i;
+   div.style.color = cars[i].color;
+   div.classList.add("stat");
+   statistics.appendChild(div);
 }
-
 
 let roadBorders = [];
-
 const target = world.markings.find((m) => m instanceof Target);
 if (target) {
-    world.generateCorridor(myCar, target.center, true);
-    roadBorders = world.corridor.borders.map((s) => [s.p1, s.p2]);
+   world.generateCorridor(myCar, target.center, true);
+   roadBorders = world.corridor.borders.map((s) => [s.p1, s.p2]);
 } else {
-    roadBorders = world.roadBorders.map((s) => [s.p1, s.p2]);
-
+   roadBorders = world.roadBorders.map((s) => [s.p1, s.p2]);
 }
 
-let frameCount = 0;
+const miniMapGraph = new Graph([], world.corridor.skeleton);
+const miniMap = new MiniMap(miniMapCanvas, miniMapGraph, rightPanelWidth, cars);
 
+let frameCount = 0;
 let started = false;
 
 
 startCounter();
 animate();
 
-function save() {
+function save(){
     localStorage.setItem("bestBrain",
         JSON.stringify(myCar.brain));
 }
 
-function discard() {
+function discard(){
     localStorage.removeItem("bestBrain");
 }
 
-function generateCars(N, type) {
+function generateCars(N, type){
     const startPoints = world.markings.filter((m) => m instanceof Start);
     const startPoint = startPoints.length > 0
-        ? startPoints[0].center
-        : new Point(100, 100);
+      ? startPoints[0].center
+      : new Point(100, 100);
     const dir = startPoints.length > 0
-        ? startPoints[0].directionVector
-        : new Point(0, -1);
+      ? startPoints[0].directionVector
+      : new Point(0, -1);
     const startAngle = - angle(dir) + Math.PI / 2;
-
-    const cars = [];
-    for (let i = 1; i <= N; i++) {
+    
+    const cars=[];
+    for(let i=1;i<=N;i++){
         const color = type == "AI" ? getRandomColor() : "blue";
         const car = new Car(
-
-            startPoint.x,
+            startPoint.x, 
             startPoint.y,
             30,
             50,
@@ -107,7 +97,7 @@ function generateCars(N, type) {
             3,
             color
         );
-        car.name = type == "AI" ? "AI" + 1 : "Me"
+        car.name = type == "AI" ? "AI" + i : "Me";
         car.load(carInfo);
         cars.push(car);
     }
@@ -115,65 +105,96 @@ function generateCars(N, type) {
 }
 
 function updateCarProgress(car) {
-    if (!car.finishTime) {
-        car.progress = 0;
-        const carSeg = getNearestSegment(car, world.corridor.skeleton);
-        for (let i = 0; i < world.corridor.skeleton.length; i++) {
-            const s = world.corridor.skeleton[i];
-
-            if (s.equals(carSeg)) {
-                const proj = s.projectPoint(car);
-
-                const firstPartOfSegment = new Segment(s.p1, proj.point);
-
-                car.progress += firstPartOfSegment.length();
-                break;
-            } else {
-
-                car.progress += s.length();
-            }
-        }
-        const totalDistance = world.corridor.skeleton.reduce(
-            (acc, s) => acc + s.length(), 0
-        );
-        car.progress /= totalDistance;
-        if (car.progress >= 1) {
-            car.progress = 1;
-            car.finishTime = frameCount;
-        }
+   if (!car.finishTime) {
+      car.progress = 0;
+      const carSeg = getNearestSegment(car, world.corridor.skeleton);
+      for (let i = 0; i < world.corridor.skeleton.length; i++) {
+         const s = world.corridor.skeleton[i];
+         if (s.equals(carSeg)) {
+            const proj = s.projectPoint(car);
+            const firstPartOfSegment = new Segment(s.p1, proj.point);
+            car.progress += firstPartOfSegment.length();
+            break;
+         } else {
+            car.progress += s.length();
+         }
+      }
+      const totalDistance = world.corridor.skeleton.reduce(
+         (acc, s) => acc + s.length(), 0
+      );
+      car.progress /= totalDistance;
+      if (car.progress >= 1) {
+         car.progress = 1;
+         car.finishTime = frameCount;
+         if (car == myCar) {
+            taDaa();
+         }
+      }
     }
-
 }
 
 function startCounter() {
-    counter.innerText = "3";
-    beep(400);
-    setTimeout(() => {
-        counter.innerText = "2";
-        beep(400);
-        setTimeout(() => {
-            counter.innerText = "1";
-            beep(400);
+   counter.innerText = "3";
+   beep(400);
+   setTimeout(() => {
+      counter.innerText = "2";
+      beep(400);
+      setTimeout(() => {
+         counter.innerText = "1";
+         beep(400);
+         setTimeout(() => {
+            counter.innerText = "GO!";
+            beep(700);
             setTimeout(() => {
-                counter.innerText = "GO!";
-                beep(700);
-                setTimeout(() => {
-                    counter.innerText = "";
-                    started = true;
-                    frameCount = 0;
-                    myCar.engine = new Engine();
-                }, 1000);
+               counter.innerText = "";
+               started = true;
+               frameCount = 0;
+               myCar.engine = new Engine();
             }, 1000);
-        }, 1000);
-    }, 1000);
+         }, 1000);
+      }, 1000);
+   }, 1000);
 }
 
-function animate() {
-    if (started) {
+function handleCollisionWithRoadBorder(car) {
+   const seg = getNearestSegment(car, world.corridor.skeleton);
 
-        for (let i = 0; i < cars.length; i++) {
-            cars[i].update(roadBorders, []);
-        }
+   const correctors = car.polygon.map((p) => {
+      const proj = seg.projectPoint(p);
+      const projPoint = proj.offset < 0
+         ? seg.p1
+         : proj.offset > 1 
+            ? seg.p2
+            : proj.point;
+      return subtract(projPoint, p);
+   });
+
+   const maxMagnitude = Math.max(...correctors.map((p) => magnitude(p)));
+   const corrector = correctors.find((p) => magnitude(p) == maxMagnitude);
+   const normCorrector = normalize(corrector);
+
+   if(corrector == correctors[0] || corrector == correctors[2]) {
+      car.angle += 0.1;
+   } else {
+      car.angle -= 0.1;
+   }
+
+   car.x += normCorrector.x;
+   car.y += normCorrector.y;
+   car.damaged = false;
+}
+
+function animate(){
+    if (started) {
+      for(let i=0;i<cars.length;i++){
+         cars[i].update(roadBorders,[]);
+      }
+    }
+
+    for (const car of cars) {
+      if (car.damaged) {
+         handleCollisionWithRoadBorder(car);
+      }
     }
 
     world.cars = cars;
@@ -186,26 +207,27 @@ function animate() {
     const viewPoint = scale(viewport.getOffset(), -1);
     //world.draw(carCtx, viewPoint, false);
     miniMap.update(viewPoint);
+    miniMapCanvas.style.transform = "rotate(" + myCar.angle + "rad)";
 
     for (let i = 0; i < cars.length; i++) {
-        updateCarProgress(cars[i]);
+      updateCarProgress(cars[i]);
     }
+
     cars.sort((a, b) => b.progress - a.progress);
 
     for (let i = 0; i < cars.length; i++) {
-        const stat = document.getElementById("stat_" + i);
-        stat.style.color = cars[i].color;
-        stat.innerText = (i + 1) + ": " + cars[i].name + (cars[i].damaged ? " 💥" : "");
-        stat.style.backgroundColor = cars[i].type == "AI" ? "black" : "white";
-        if (cars[i].finishTime) {
-            stat.innerHTML += "<span style='float:right;'>" +
-
-                (cars[i].finishTime / 60).toFixed(1) + "s </span>"
-        }
-
+      const stat = document.getElementById("stat_" + i);
+      stat.style.color = cars[i].type == "AI" ? "white" : cars[i].color;
+      stat.innerText = (i+1) + ": " + cars[i].name + (cars[i].damaged ? " 💀" : "");
+      stat.style.backgroundColor = cars[i].type == "AI" ? "black" : "white";
+      if (cars[i].finishTime) {
+         stat.innerHTML += "<span style='float:right;'>" +
+            (cars[i].finishTime / 60).toFixed(1) + "s </span>"
+      }
     }
+
     camera.move(myCar);
-    camera.draw(carCtx);
+    //camera.draw(carCtx);
     camera.render(cameraCtx, world);
 
     frameCount++;
